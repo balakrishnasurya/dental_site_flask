246aca541aea567c947b6d4245590bb1ab5e562f
balakrishnasurya <90981476+balakrishnasurya@users.noreply.github.com>
2026-04-26 14:14:18 +0530

    made all the file local to replace easily

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template
+import json
+import os
 
 app = Flask(__name__)
 
-# This route tells Flask what to load when someone visits the main URL (/)
 @app.route('/')
-def home():
-    # Flask automatically looks in the "templates" folder for this file
-    return render_template('index.html')
+def index():
+    # Construct the path to the JSON file
+    json_path = os.path.join(app.root_path, 'data.json')
+    
+    # Read the JSON file
+    with open(json_path, 'r', encoding='utf-8') as file:
+        site_data = json.load(file)
+        
+    # Pass the data to the template
+    return render_template('index.html', data=site_data)
 
 if __name__ == '__main__':
-    # debug=True allows the server to auto-reload if you make changes to your code
     app.run(debug=True)
